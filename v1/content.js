7b86651e9738cf6c9dddf6f0e95e63700702cd3e
yoni <yoni.bmesh@gmail.com>
2009-02-21 20:21:20 +0000

    added v1 of howtohearasentence to the repository.

---
+++ v1/content.js
@@ -0,0 +1,202 @@
+/* These are the sentences to be used in the performance.
+ * Sentence names are of the form: {READER}{Sentence number}{highlighted}
+ */ 
+var SENTENCES =[
+[0,2,"I think <b>objects</b> are usually small enough to hold, but this is only because my mind catalogs what it touches.",
+"I think objects are usually small enough to hold, but this is only because my <b><font color=#0000FF>mind</font></b> catalogs what it touches."
+],
+[0,3,"To start asking one question or formulating one set of thoughts, is <b>contingent</b> upon many other questions, many other thoughts.",
+"To <b><font color=#FF8C00>start</font></b> asking one question or formulating one set of thoughts, is contingent upon many other questions, many other thoughts.",
+"To <b><font color=#FF8C00>start</font></b> asking one question or formulating one set of thoughts, is contingent upon many other questions, <b><font color=#006400>many</font></b> other thoughts."
+],
+[0,1,"I often wonder if these critiques of social <b>order</b> could also be solutions."
+],
+[0,2,"The gun, which should be hung on the wall, in Chekhovian terms, wasn't - it was introduced as an <b>artificial</b> external element when it was narratively necessary, and the axe, which is introduced prominently but not does not return in the source text, becomes the instrument of, as the poem suggests, both escape and death, which are conflated and do not contain clear boundaries.",
+"The gun, which should be hung on the wall, in Chekhovian terms, wasn't - it was introduced as an artificial external element when it was narratively necessary, and the axe, which is introduced prominently but not does not return in the source text, becomes the instrument of, as the poem <b><font color=#8B2323>suggests</font></b>, both escape and death, which are conflated and do not contain clear boundaries."
+],
+[0,1,"On the days that I do not monitor your use of <b>language</b>, I forget a little how you see the world; I forget how the world guides you."
+],
+
+
+[0,2,"I am concerned that, as you've pointed out in the past, all this legalizing will <b>prove</b> fatal.",
+"I am concerned that, as you've pointed out in the <b><font color=#EEB422>past</font></b>, all this legalizing will prove fatal."
+],
+[0,2,"We absorbed the <b>event</b> because we believed in its ability to serve as evidence.",
+"We absorbed the <b><font color=#DC132C>event</font></b> because we believed in its ability to serve as evidence."
+],
+[0,1,"Although the project has <b>generated</b> significant interest, it has no immediate prospect for turning a profit."
+],
+[0,1,"As we are <b>talking</b>, we are announcing our agenda, and I get a fear of proclamation again."
+],
+[0,1,"Since your content is <b>stored</b> on Google's secure servers, even if something happens to your hard drive, your documents are protected."
+],
+
+
+[0,1,"When I <b>speak</b>, I try to avoid listening outside of myself, or listening for others."
+],
+[0,3,"When we returned to <b>speech</b> and eye contact, I found they had personalities and histories.",
+"When we returned to speech and eye contact, I found they had personalities and <b><font color=#F08080>histories</font></b>.",
+"When we returned to speech and <b><font color=#9932CC>eye</font></b> contact, I found they had personalities and <b><font color=#F08080>histories</font></b>."
+],
+[0,1,"His <b>hearing</b> lost, his body became a truer isolation, and his vulnerability preceded him in foreign spaces."
+],
+[0,2,"With song, dance, projections, sound effects, and <b>mimicry</b> of preposterous acts, visions and revisions of characters, media, genres and events will change, interrupt, and harmonize with each other.",
+"With song, dance, projections, sound effects, and mimicry of preposterous acts, visions and revisions of characters, media, genres and <b><font color=#DC132C>events</font></b>will change, interrupt, and harmonize with each other."
+],
+[0,1,"<b>Everything</b> cannot cover multiple things, but it serves a very nice purpose on its own."
+],
+
+
+[0,1,"I feel like you are <b>backing</b> off very big questions about gaze here."
+],
+[0,2,"The <b>past</b> is available for comparison to the present, and it forms speculation of the future.",
+"The <b><font color=#EEB422>past</font></b> is available for comparison to the present, and it forms speculation of the future."
+],
+[0,1,"I am advocating a Lean <b>Hypothesis</b> about reality and a Lean Alternative to our materialistic culture."
+],
+[0,3,"The equation <b>suggests</b> that we have distilled cumulative experience there are absolute answers, but we may need to look to many things at once.",
+"The equation <b><font color=#8B2323>suggests</font></b> that we have distilled cumulative experience there are absolute answers, but we may need to look to many things at once.",
+"The equation <b><font color=#8B2323>suggests</font></b> that we have distilled cumulative experience there are absolute answers, but we may need to look to <b><font color=#006400>many</font></b> things at once."
+],
+[0,1,"I think cross-identification is quite powerful: I live on it, as you know, in terms of sexual <b>orientation</b> and gender identity, but also in terms of art making disciplines and approaches."
+],
+
+
+[0,2,"In order to <b>witness</b> change, we must see a difference in time or in us.",
+"In order to witness change, we must see a difference in <b><font color=#00008B>time</font></b> or in us."
+],
+[0,2,"Maybe we want to specify that the event does not need to be quiet, but that we would like a distinct space which is shielded from the <b>noise</b>.",
+"Maybe we want to specify that the event does not need to be quiet, but that we would like a distinct <b><font color=#B452CD>space</font></b> which is shielded from the noise."
+],
+[0,1,"I wish I could get out of this <b>means</b> to an end."
+],
+[0,1,"What could make someone leave this beautiful, spacious apartment, <b>situated</b> in the heart of Logan Square on historic Kedzie Blvd, part of Chicago's system of boulevards modeled on the boulevards of Paris?"
+],
+[0,1,"A <b>map</b> is not just a visualization of an area: it is a tool for imagining our path to the future, allowing us to visualize ourselves in the continuous present of the world."
+],
+
+
+[0,1,"At the point of checking in on the solidity of anything - self, other, any moving, operational structure - it <b>shifts</b>, changes, slips away and becomes other than what it was, what one might assume it to be."
+],
+[0,2,"<b>Purpose</b> is the perfect illusion to help us start this over every day.",
+"Purpose is the perfect illusion to help us <b><font color=#FF8C00>start</font></b> this over every day."
+],
+[0,2,"The evening was maneuvered to a <b>bridge</b> over the Chicago River, lights twinkling around us, our eyes meeting in the cold night air, and I remarked how I should probably be getting home.",
+"The evening was maneuvered to a bridge over the Chicago River, lights twinkling around us, our eyes meeting in the cold <b><font color=#2F4F4F>night</font></b>air, and I remarked how I should probably be getting home."
+],
+[0,3,"When I first laid eyes on the <b>models</b>, I questioned its ability to help us see from the inside-out.",
+"When I first laid eyes on the models, I questioned its ability to <b><font color=#B8860B>help</font></b> us see from the inside-out.",
+"When I first laid <b><font color=#9932CC>eyes</font></b>on the models, I questioned its ability to <b><font color=#B8860B>help</font></b> us see from the inside-out."
+],
+[0,2,"We don't necessarily have to go all out if time seems too <b>constrained</b>, just scarf some produce on the grass.",
+"We don't necessarily have to go all out if <b><font color=#00008B>time</font></b>seems too constrained, just scarf some produce on the grass."
+],
+
+
+[0,2,"He <b>holds</b> onto feeling as an answer, while she is hooked on the social histories of their dynamic.",
+"He holds onto feeling as an answer, while she is hooked on the social <b><font color=#F08080>histories</font></b> of their dynamic."
+],
+[0,1,"Right now there is a small-scale agricultural program which provides minimal food to the residents and <b>prototypes</b> some agricultural systems which could be applied at a larger scale."
+],
+[0,1,"If we are ever going to cross the line into the sciences, we are going to have to embrace the utility of the <b>formal</b> proof."
+],
+[0,2,"I remember saying it along with a barrage of other things about the beach and the reactions to the war there and I should have known that Aaron was going to blow it out of proportion, as he became immediately fixated on it, though it was something I said rather offhandedly and, to my mind, with no <b>implied</b> judgment.",
+"I remember saying it along with a barrage of other things about the beach and the reactions to the war there and I should have known that Aaron was going to blow it out of proportion, as he became immediately fixated on it, though it was something I said rather offhandedly and, to my <b><font color=#0000FF>mind</font></b>, with no implied judgment."
+],
+[0,2,"<b>Emphatic</b> convictions are not the things that get me up in the norning, they are the things that keep me up at night.",
+"Emphatic convictions are not the things that get me up in the norning, they are the things that keep me up at <b><font color=#2F4F4F>night</font></b>."
+],
+
+
+[0,1,"My cell phone was recently liberated from the tyranny of my ownership by a young revolutionary and efforts to retrieve it through high school back <b>channels</b> have proved futile."
+],
+[0,1,"<b>Embodiment</b> eludes me, which should be impossible."
+],
+[0,1,"These issues of marketing and <b>distribution</b> are key, of course, to what is independent anymore."
+],
+[0,1,"All desire to solve problems begin with the <b>experiential</b>."
+],
+[0,2,"This is the history of <b>bootstrapping</b> or booting, which began in the 1880s as a leather strap and evolved into a group of metaphors that share a common meaning, a self-sustaining process that proceeds without external help.",
+"This is the history of bootstrapping or booting, which began in the 1880s as a leather strap and evolved into a group of metaphors that share a common meaning, a self-sustaining process that proceeds without external <b><font color=#B8860B>help</font></b>."
+],
+
+
+[0,2,"<b>Attention</b> to the moment privileges time over space.",
+"Attention to the moment privileges time over <b><font color=#B452CD>space</font></b>."
+],
+[0,1,"Say hi to your family and the impending <b>child</b>."
+],
+[0,1,"Our collective <b>intentions</b> dictate the mood and direction for our political body."
+],
+[0,1,"I do want to take you to the woods (that's where <b>bears</b> live)."
+],
+[0,1, "If we do not <b>act</b>, then we allow nature’s spirals to continue as they will."
+],
+
+
+[0,1,"I guess the <b>differentiations</b> always come in the eye of the beholder."
+],
+[0,1,"It was his sense of <b>possibility</b> that defined the aesthetic."
+],
+[0,1,"Your influence on my vocabulary and diction is so immense that I have come to feel I can <b>draw</b> liberally from the source without citation."
+],
+[0,1,"<b>Silence</b> was her only cruelty."
+],
+[0,1,"I've been using some of my meager budget to <b>acquire</b> most of the out of print books."
+]
+];
+
+
+
+var INF =[
+[0,0,"We need to distinguish the <b><font color=#DC132C>event</font></b> from the timeline in order to catalog our history.",
+],
+[0,0,"Remembering the <b><font color=#EEB422>past</font></b> elicits fear of repetition.",
+],
+[0,0,"Any singular piece of knowledge is determined by <b><font color=#006400>many</font></b> avenues of inquiry.",
+],
+[0,0,"Our tools for discovering facts and poetics represent the potential of <b><font color=#8B2323>suggestion</font></b> rather than the potential of certainty.",
+],
+[0,0,"To start is to <b><font color=#FF8C00>start</font></b> over.",
+],
+
+
+[0,0,"Our <b><font color=#9932CC>eyes</font></b> are only as intelligent as our way of looking.",
+],
+[0,0,"When we perceive time as measured,<b><font color=#00008B>time</font></b> is differentiated.",
+],
+[0,0,"We hold our <b><font color=#F08080>histories</font></b> and history holds us.",
+],
+[0,0,"The <b><font color=#0000FF>mind</font></b> generates its own assumptions.",
+],
+[0,0,"Serious shit comes up at <b><font color=#2F4F4F>night</font></b>.",
+],
+
+
+[0,0,"We tend to believe that <b><font color=#B8860B>helps</font></b> agency originates outside the self.",
+],
+[0,0,"<b><font color=#B452CD>Space</font></b> protects information; time retrieves clarity."
+]
+];
+
+var Pairs =[
+[6,13,1,false],
+[5,16,2,false],
+[1,18,3,false],
+[3,18,4,false],
+[1,26,5,false],
+[11,28,6,false],
+[29,20,7,false],
+[11,30,8,false],
+[33,0,9,false],
+[27,34,10,false],
+[39,28,11,false],
+[21,40,12,false]
+]
+
+
+
+
+
+
+
