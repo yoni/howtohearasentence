b153367952a9cd518e7cd4415057762ba4dab0a1
yoni <yoni.bmesh@gmail.com>
2009-06-07 18:18:21 +0000

    Added audio to page. Need to test.

--- v1.1/lib/hthas/hthas_audio.js
+++ v1.1/lib/hthas/hthas_audio.js
@@ -5,14 +5,13 @@
  * @author yoni.ben-meshulam
  */
 // the positions in the audio file that have keywords
+var queuePositions;
+// the positions in the audio file that have keywords
 var queuePositions = [15,20,25,30,35,40,45,50,55,60];
 var queue = [];
 for(var i in queuePositions) {
 	queue[queuePositions[i]] = true;
 }
-
-
-
 function createPlayer() {
     var flashvars = {
             file:"http://omnib.in/hthas/v1.1/audio/we_record_on_monday4.mp3", 
@@ -30,7 +29,7 @@ function createPlayer() {
 }
 
 /**
- * We attachd this listener to the player. It receives the time info every 1/10 of a second.
+ * We attached this listener to the player. It receives the time info every 1/10 of a second.
  * @param {duration:'a number',position:'a number'}
  */
 function timeListener(obj) {
