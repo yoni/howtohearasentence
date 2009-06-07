1626ac794dea5e4e89386d2de7e99ab9e5e0c50c
yoni <yoni.bmesh@gmail.com>
2009-06-07 15:35:22 +0000

    Changed json structure a bit.

--- v2/hthas/views.py
+++ v2/hthas/views.py
@@ -32,7 +32,7 @@ def tagsentences(request):
     sentences = json.loads(json_sentences)
     html=""
     for sentence in sentences:
-        text = sentence['sentence']
+        text = sentence['text']
         html+="For sentence: <br><a class='rawSent'>" + text + "</a><br>"
         html+="  The tagged parts of speech are:<br><a class='taggedSent'>" + str(pos_tag(word_tokenize(text)))+"</a>"
         html+="<hr>"
@@ -49,59 +49,4 @@ def posTaggerPage(request):
 def home(request):
     t = get_template('hthas/home.html')
     html = t.render(Context({}))
-    return HttpResponse(html)
-
-
-# The rest of these views are simple experiments, and can probably be removed,
-
-# simple page that uses the dojo js framework
-def dojoHelloWorld(request):
-    t = get_template('hthas/dojoHelloWorld.html')
-    html = t.render(Context({}))
-    return HttpResponse(html)
-
-# from chapter 3 of the nltk book
-def dostoevsky(request):
-    url = "http://www.gutenberg.org/files/2554/2554.txt"
-    raw = urlopen(url).read()
-    html = raw[:75]
-    tokens = nltk.word_tokenize(raw)
-    tokens[:10]
-    text = nltk.Text(tokens)
-    type(text)
-    text[1020:1060]
-    text.collocations()
-    raw.find("PART I")
-    raw.rfind("End of Project Gutenberg's Crime")
-    raw = raw[5303:1157681]
-    return HttpResponse(raw.find("PART I"))
-
-# from chapter 3 of the nltk book
-def bbc(request):
-    url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
-    html = urlopen(url).read()
-    return HttpResponse(html)
-
-# recognizing an ajax request vs. a regular request
-def thingsaregreat(request):
-    if request.method == 'POST':
-        article = glob + request.POST.__getitem__("phrase")
-    if request.is_ajax():
-        return HttpResponse(article)
-    return HttpResponse("This page is meant to be called using AJAX")
-
-#experimenting with grabbing a sentence from the request and doing some nlp with it
-def histogram(request):
-    html=""
-    phrase=request.GET.__getitem__("phrase")
-    phraseFDist=FreqDist()
-    for word in nltk.tokenize.whitespace(phrase):
-        phraseFDist.inc(word.lower())
-    html+="For the phrase:" + str(phrase) + "<hr>"
-    html+="  The total number of words that were included in the frequency distribution is: " + str(phraseFDist.B())
-    html+="<br>"
-    html+="  The total number of occurances of the word 'the' is: " + str(phraseFDist['the'])
-    html+="<br>"
-    html+="  The tagged parts of speech are:<br>"
-    html+=str(pos_tag(word_tokenize(phrase)))
     return HttpResponse(html)
