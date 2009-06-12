313e642b19f3caf7ed608b357d66b1a57d704e56
yoni <yoni.bmesh@gmail.com>
2009-06-12 01:12:36 +0000

    * Consolidated all static javascript (mostly content, but also colors) into hthas_static.js.  * Consolidated all dynamic javascript into hthas.js.

--- v1.1/lib/hthas/hthas.js
+++ v1.1/lib/hthas/hthas.js
@@ -220,9 +220,9 @@ var hthas = {
 	 */
 	getKeywordColor:function(){
 		//for now, just getting a random color. need to change this.
-		var rand = Math.random()*keywordColors.length;
+		var rand = Math.random()*KEYWORD_COLORS.length;
 		rand = Math.floor(rand);
-		return keywordColors[rand];;
+		return KEYWORD_COLORS[rand];;
 	},
 	/**
 	 * Initializes sentence objects for later manipulation.
@@ -389,3 +389,96 @@ var hthas = {
 		queue:[]
 	}
 }
+
+/**
+ * A prototype for Sentence objects 
+ * (i.e. a recipe for making a Sentence)
+ * args must be an object literal (i.e. a map of key:value pairs).
+ * It supports the following keys:
+ * text, position, type
+ * 
+ * Usage:
+ * var sentence = new Sentence(
+ *     {
+ *     		text:'My Sentence goes like this', 
+ *     		position:{x:'100',y:'100'},
+ *     		type:'left'
+ * 	   });
+ * 
+ * text: any text value, supports embedded html.
+ * type: left/right, depending on which speaker wrote the sentence
+ * 
+ * @param {Object} args
+ * @author yoni
+ */
+Sentence = function(args){
+	//set the member values, according to the constructor parameters
+	this.text = args.text;
+	this.position = args.position;
+	this.type = args.type;
+
+	/***************************************************************
+	 * Set up document element for the Sentence
+	 **************************************************************/
+	//create a document element that will contain our text on screen
+	this.element = document.createElement("span");
+	
+	// set up the document element
+	this.element.id = args.id;
+	this.element.innerHTML = this.text;
+	this.element.className = "sentence";
+	this.element.style.display = 'inline';
+
+	//the initial position of the sentence on the screen
+	this.element.style.bottom = this.position.y;
+	if(this.type == 'left') {
+		this.element.style.left = this.position.x;
+	} else {
+		this.element.style.right = this.position.x;
+	}
+	//add element to the document
+	document.body.appendChild(this.element);
+}
+
+/**
+ * A prototype for Inference objects 
+ * (i.e. a recipe for making an Inference)
+ * args must be an object literal (i.e. a map of key:value pairs).
+ * It supports the following keys:
+ * text, position, type
+ * 
+ * Usage:
+ * var sentence = new Sentence(
+ *     {
+ *     		text:'My Sentence goes like this', 
+ *     		position:{x:'100',y:'100'},
+ *     		type:'left'
+ * 	   });
+ * 
+ * text: any text value, supports embedded html.
+ * type: left/right, depending on which speaker wrote the sentence
+ * 
+ * @param {Object} args
+ * @author yoni
+ */
+Inference = function(args){
+	//set the member values, according to the constructor parameters
+	this.text = args.text;
+	this.position = args.position;
+	this.type = args.type;
+
+	/***************************************************************
+	 * Set up document element for the Inference
+	 **************************************************************/
+	//create a document element that will hold our text on screen
+	this.element = document.createElement("span");
+	
+	// set up the document element
+	this.element.id = args.id;
+	this.element.innerHTML = this.text;
+	this.element.className = "inference";
+	this.element.style.display = 'none';
+
+	//add element to the document
+	document.body.appendChild(this.element);
+};
